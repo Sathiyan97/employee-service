74e50b277d273f0cd7750515192fa8b52ba6a23a
Sathiyan <sathiyan97@gmail.com>
2022-10-13 13:03:46 +0530

    Implement Generic Exception Handling for All Controllers

---
+++ src/main/java/com/example/employeeservice/exception_handler/EmployeeGlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.employeeservice.exception_handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice
+public class EmployeeGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+        EmployeeExceptionResponse exception = new EmployeeExceptionResponse(ex.getMessage(),
+                request.getDescription(false), new Date());
+
+       return new ResponseEntity<Object>(exception, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+}
